e26cd0429d709d6d23275f8999859d847cc66401
EduardToth <48791970+EduardToth@users.noreply.github.com>
2019-08-14 10:36:44 +0300

    "looks good"

--- solution.js
+++ solution.js
@@ -2,44 +2,80 @@
 
 var languages = ['de', 'en', 'es', 'fr', 'it', 'ja', 'nl'];
 
-	
+function generateTabs( nrTabs )
+{
+	 if( nrTabs == 0)
+		 return "";
+	 else
+		 return generateTabs( nrTabs - 1) + '\t';
+}
 
-function processArray( arr, languageModel, englishModel ) {
+function processArray( arr, languageModel, englishModel, nrTabs) {
 	let innerJSON = '{\n';
 	for(var i=0; i < arr.length - 1; i++)
-		innerJSON += englishModel[ arr[ i ] ] + ":" + languageModel[ arr[ i ] ] + ',\n';
-	innerJSON += englishModel[ arr[ i ] ] + ":" + languageModel[ arr[ i ] ];
-	innerJSON += '\n}';
+		innerJSON += generateTabs( nrTabs ) + '"' + englishModel[ arr[ i ] ] + '":"' + languageModel[ arr[ i ] ] + '",\n';
+	innerJSON += generateTabs( nrTabs ) + '"' + englishModel[ arr[ i ] ] + '":"' + languageModel[ arr[ i ] ] + '"';
+	innerJSON += generateTabs( nrTabs ) + '\n' + generateTabs( nrTabs ) + '}';
 	
 	return innerJSON;
 }
-	
-function iterateModelJSON(model, languageModel, englishModel) {
 
+function createTheAlmostMostImportantJSON(principalKey, arr, languageModel, englishModel, nrTabs)
+{
+	let output =   '   {"' + principalKey + '":';
+
+	output += processArray(arr, languageModel, englishModel, nrTabs + 1);
+
+	return output + '\n' + generateTabs( nrTabs ) + '}';
+}
+	
+function iterateModelJSON(model, languageModel, englishModel, nrTabs) {
+
+    let partialString = '';
+	
 	for(var itr in model)
 		if( Array.isArray( model[ itr ] ) )
-			console.log( processArray(model[ itr ], languageModel, englishModel ) + '\n' );
+			 partialString +=  createTheAlmostMostImportantJSON(itr, model[ itr ], languageModel, englishModel, nrTabs + 1) + ',\n';
+	return partialString.substring(0, partialString.length - 2);
 }
 
 function createSecondJSON()
 {
+	let finalJSON = ''; 
+	
 	const fs = require('fs');
 	
+	//Here I read the model file
 	let rawData = fs.readFileSync('C:\\Users\\User\\Desktop\\myVersion\\plugin\\metaTranslations\\model.json');
     let model = JSON.parse( rawData );
 	
+	//Here I read the english translation
 	let englishRawData = fs.readFileSync('C:\\Users\\User\\Desktop\\myVersion\\translatedFiles\\en\\supervisor.json');
     let englishModel = JSON.parse( englishRawData );
 	
-	for(var i=0; i < languages.length; i++) {
+	//Here I iterate every language and I create the JSONs
+	for(var i=0; i < languages.length - 1; i++) {
 		let languageRawData = fs.readFileSync('C:\\Users\\User\\Desktop\\myVersion\\translatedFiles\\' +  languages[ i ] + '\\supervisor.json');
 	    let languageModel = JSON.parse( languageRawData );
 		
-		iterateModelJSON(model, languageModel, englishModel);
+		finalJSON += '  "' +  languages[ i ] + '": ' + iterateModelJSON(model, languageModel, englishModel, 1) + ",\n";
 	}	
+	
+	let languageRawData = fs.readFileSync('C:\\Users\\User\\Desktop\\myVersion\\translatedFiles\\' +  languages[ languages.length - 1 ] + '\\supervisor.json');
+	let languageModel = JSON.parse( languageRawData );
+	finalJSON += '  "' + languages[ languages.length - 1] + '": ' + iterateModelJSON(model, languageModel, englishModel, 1);
+	
+	return '{\n' + finalJSON + '\n}';
 }
 
-createSecondJSON();
+const fs = require('fs');
+fs.writeFile("test.json", createSecondJSON(), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
 
 
 
